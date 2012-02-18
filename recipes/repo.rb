10c5de7b0f4d1699ec862e1159e93db1e0e256a6
Cliff Erson <cerson@Calculon.local>
2012-02-17 17:58:57 -0800

    added repo recipe

---
+++ recipes/repo.rb
@@ -0,0 +1,13 @@
+
+execute "yum makecache" do
+  action :nothing
+end
+
+template "/etc/yum.repos.d/cloudera.repo" do
+  owner "root"
+  mode "0644"
+  #source "cloudera.repo.erb"
+  notifies :run, resources("execute[yum makecache]"), :immediately
+end
+
+
