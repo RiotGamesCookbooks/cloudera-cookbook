c7ca3fddb4e6d2d1d0f6cf779d69ddfec4cc4cf5
Cliff Erson <cerson@Calculon.local>
2012-02-23 19:23:35 -0800

    moved site files to generic template

--- recipes/default.rb
+++ recipes/default.rb
@@ -53,11 +53,27 @@ core_site_vars = { :options => node[:hadoop][:core_site] }
 core_site_vars[:options]['fs.default.name'] = "hdfs://#{namenode[:ipaddress]}:54310" if namenode
 
 template "/etc/hadoop-#{node[:hadoop][:version]}/conf.chef/core-site.xml" do
+  source "generic-site.xml.erb"
   mode 0750
   owner "hdfs"
   group "hdfs"
   action :create
   variables core_site_vars
+end
+
+hdfs_site_vars = { :options => node[:hadoop][:core_site] }
+hdfs_site_vars[:options]['fs.default.name'] = "hdfs://#{namenode[:ipaddress]}:54310" if namenode
+
+# TODO this template needs the secondary name node searched, key dfs.secondary.http.address
+# hostname:port
+
+template "/etc/hadoop-#{node[:hadoop][:version]}/conf.chef/hdfs-site.xml" do
+  source "generic-site.xml.erb"
+  mode 0750
+  owner "hdfs"
+  group "hdfs"
+  action :create
+  variables hdfs_site_vars
 end
 
 template "/etc/hadoop-#{node[:hadoop][:version]}/conf.chef/hadoop-env.sh" do
@@ -75,6 +91,7 @@ template "/etc/hadoop-#{node[:hadoop][:version]}/conf.chef/fair-scheduler.xml" d
   action :create
   variables node[:hadoop][:fair_scheduler]
 end
+
 
 # TODO  remove this shit and templitize the recipes
 # Copy the riot settings over - we dont want this - move to roles via array of hashes
