79a25648fd7444aca70c5290ca6d38aa6953e19e
Istvan <iszukacs@riotgames.com>
2012-06-04 11:37:17 -0700

    Adding topology to the jobtracker

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cerson@me.com"
 license          "Apache 2.0"
 description      "Installs and configures cloudera (hadoop/hive)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.6"
+version          "0.0.7"
 
 %w{ centos redhat fedora }.each do |os|
   supports os
