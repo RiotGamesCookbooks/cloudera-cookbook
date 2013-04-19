8c8effb04a1d3de09031759d2762fd75b0bda7c0
Chris Gianelloni <cgianelloni@bluekai.com>
2013-04-19 11:48:44 -0700

    Make the topology script stuff optional

--- recipes/default.rb
+++ recipes/default.rb
@@ -160,25 +160,26 @@ template "#{chef_conf_dir}/slaves" do
   variables( :nodes => slaves )
 end
 
-topology = { :options => node[:hadoop][:topology] }
+if node[:hadoop][:hdfs_site]['topology.script.file.name']
+  topology = { :options => node[:hadoop][:topology] }
+  topology_dir = File.dirname(node[:hadoop][:hdfs_site]['topology.script.file.name'])
 
-topology_dir = File.dirname(node[:hadoop][:hdfs_site]['topology.script.file.name'])
+  directory topology_dir do
+    mode 0755
+    owner "hdfs"
+    group "hdfs"
+    action :create
+    recursive true
+  end
 
-directory topology_dir do
-  mode 0755
-  owner "hdfs"
-  group "hdfs"
-  action :create
-  recursive true
-end
-
-template node[:hadoop][:hdfs_site]['topology.script.file.name'] do
-  source "topology.rb.erb"
-  mode 0755
-  owner "hdfs"
-  group "hdfs"
-  action :create
-  variables topology
+  template node[:hadoop][:hdfs_site]['topology.script.file.name'] do
+    source "topology.rb.erb"
+    mode 0755
+    owner "hdfs"
+    group "hdfs"
+    action :create
+    variables topology
+  end
 end
 
 hadoop_tmp_dir = File.dirname(node[:hadoop][:core_site]['hadoop.tmp.dir'])
